a37b357f69d8e27bb6a216c159008bb2566d8496
anncisz <anncisz@gmail.com>
2019-06-22 10:50:58 +0200

    first commit

---
+++ src/main/java/com/sda/group/family_apps/FamilyAppsApplication.java
@@ -0,0 +1,13 @@
+package com.sda.group.family_apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyAppsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyAppsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/group/family_apps/FamilyAppsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sda.group.family_apps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FamilyAppsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
