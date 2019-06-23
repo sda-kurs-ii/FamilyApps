1d24846ae727d3e8fd63bd4455fd6507d1e1a90a
Krzysztof.Pawelec@tomtom.com <klopr@interia.pl>
2019-06-23 09:52:14 +0200

    refactor

--- src/main/java/com/sda/group/family_apps/games/DobbleController.java
+++ src/main/java/com/sda/group/family_apps/games/DobbleController.java
@@ -1,4 +1,4 @@
-package com.sda.group.family_apps.Games;
+package com.sda.group.family_apps.games;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
